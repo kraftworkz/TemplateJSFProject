e5ce367998487ea067156384ba0bbd7d6368d5d2
Engin YILDIZ <Engin@Engins-MacBook-Pro.local>
2015-06-14 18:38:25 +0300

    First Commit

---
+++ src/main/java/com/kraftworkz/bean/RegisterEmployee.java
@@ -0,0 +1,45 @@
+package com.kraftworkz.bean;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+import com.kraftworkz.domain.Employee;
+import com.kraftworkz.service.EmployeeService;
+
+@ManagedBean
+@SessionScoped
+public class RegisterEmployee {
+
+	@ManagedProperty("#{employeeService}")
+	private EmployeeService employeeService;
+
+	private Employee employee = new Employee();
+
+	public EmployeeService getEmployeeService() {
+		return employeeService;
+	}
+
+	public void setEmployeeService(EmployeeService employeeService) {
+		this.employeeService = employeeService;
+	}
+
+	public Employee getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+
+	public String register() {
+		// Calling Business Service
+		employeeService.register(employee);
+		// Add message
+		FacesContext.getCurrentInstance().addMessage(null, 
+				new FacesMessage("The Employee "+this.employee.getEmployeeName()+" Is Registered Successfully"));
+		return "";
+	}
+}
